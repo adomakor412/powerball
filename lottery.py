194081c9e6c9e8d0303a35119195eab09dcaa729
Ronald Adomako <ronaldadomako@icloud.com>
2018-07-21 03:44:17 -0400

    code review and suggested tickets

--- lottery.py
+++ lottery.py
@@ -1,58 +1,54 @@
 # Choose the lottery numbers
 # lottery.py by Ronald Adomako July 7, 2016
+
+from itertools import combinations
+import string
 import timeit
 start_time = timeit.default_timer()
 
-from itertools import combinations
-
 class lotteryDatabase:
-    #Generate pool of possible numbers
-    '''
-    pool = []
-    for first in range(1,70):
-        for second in range (first+1,70):
-            for third in range(second+1,70):
-                for fourth in range(third+1,70):
-                    for fifth in range(fourth+1,70):
-                        draw=[first, second, third, fourth, fifth]
-                        pool.append(draw)
-    '''
     def __init__(self):       
         self.histBallCount = [0]*69
         self.histDraw = []       
         self.pool = list(combinations(list(range(1,70)),5))
-        #len('self.pool') > len('lotteryDatabase.pool')
-        self.optPool = []
+        self.optPool = []#pool of undrawn numbers
+        self.played = []#pool of played tickets
          
-    def genHistDraw(self, draws):       
+    def genHistDraw(self, draws):
+        
         for line in draws:
             line = line.strip()
             date, *nums = line.split() #python 3
             for i in range(5):
                 ball = int(nums[i])
-                self.histBallCount[ball-1]+=1
-        elapsed = timeit.default_timer() - start_time
-        print(elapsed, 'seconds?')        
+                self.histBallCount[ball-1]+=1 #tally balls since 10/07/2015      
         return self.histBallCount
 
-    def pickDrawing(self):
+    def pickDrawing(self):#tally of least 5 picked balls
+        tally = self.histBallCount
         minDraws = []
-        for i in range(69):
-            if i==0:
-                minDraws[i] = i+1
-            elif self.histBallCount[i-1] < self.histBallCount[i]:
-                minDraws.insert(i, i+1)
-            else:
-                #handle repeated or tied values
-                maxDrawValues = self.histBallCount[minDraws[-1]] 
-                indexMaxDrawValues = self.histBallCount.index(maxDrawValues)
-                minDraws.insert(indexMaxDrawValues,i+1)  
+        for count in range(69):
+            minTally = min(tally)
+            ball = tally.index(minTally)+1 #ball draw corresponding to count
+            d=dict()
+            d[ball]= minTally
+            minDraws.append(d)
+            tally[ball-1]=1000 #place holder for indices for all tallies <1000
+        picks = 5
+        self.optPool = minDraws[:picks]
+        while minDraws[picks][list(minDraws[picks].keys())[0]] == minDraws[picks-1][list(minDraws[picks-1].keys())[0]]:
+            picks+=1
+            self.optPool = minDraws[:picks]
+            if picks == 70:#in the case that all the balls are uniform
+                break
+        return self.optPool
 
 def main():
+    print('Your calculation for generating wins is running.')
     lottery = lotteryDatabase()
     tally = open('powerball.txt','r')
     tallyList = lottery.genHistDraw(tally)
-    print (lottery.histBallCount)
+    print ('These are your suggested balls :with tally', lottery.pickDrawing())
     draws = open('tickets.txt','r')
 '''
 if __name__ == 'main':
